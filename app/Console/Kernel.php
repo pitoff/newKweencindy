596fb8184668edecadf9c667fda2736b53d2b3db
peter offodile <peteroffodile@gmail.com>
2023-03-09 14:44:59 +0100

    updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendEmailNotifications;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        SendEmailNotifications::class
     ];
 
     /**
@@ -24,7 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->command('send_email_notifications:sendEmailNotifications')->everyFiveMinutes()->withoutOverlapping();
+        $schedule->command('queue:work --daemon')->everyFiveMinutes()->withoutOverlapping();
     }
 
     /**
