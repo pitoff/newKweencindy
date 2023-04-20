655924738570ca3c326d6c18bc87663476c5d4ff
peter offodile <peteroffodile@gmail.com>
2023-04-20 17:24:56 +0100

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,11 +27,5 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
-        <span class="token keyword">public</span> <span class="token keyword">function</span> <span class="token function-definition function">boot</span> <span class="token punctuation">(</span> <span class="token punctuation">)</span>
-        <span class="token punctuation">{</span>
-            <span class="token keyword">if</span> <span class="token punctuation">(</span> <span class="token variable">$this</span> <span class="token operator">-&gt;</span> <span class="token property">app</span> <span class="token operator">-&gt;</span> <span class="token function">environment</span> <span class="token punctuation">(</span> <span class="token string single-quoted-string">'production'</span> <span class="token punctuation">)</span> <span class="token punctuation">)</span> <span class="token punctuation">{</span>
-                <span class="token class-name static-context">URL</span> <span class="token operator">::</span> <span class="token function">forceScheme</span> <span class="token punctuation">(</span> <span class="token string single-quoted-string">'https'</span> <span class="token punctuation">)</span> <span class="token punctuation">;</span>
-            <span class="token punctuation">}</span>
-        <span class="token punctuation">}</span>
     }
 }
